32689d9141464bd2efa2dd2d2309eb29b9c15aab
Clemuch009 <clementmuchaimoko@gmail.com>
2025-03-07 23:36:00 +0300

    exceptions

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    try:
+        for i in range(my_list_1, for j in (my_list_2):
+                       if not isinstance((i, j) , (int,float):
+                                         print ("wrong type")
+
+                       if j == 0:
+                                         print("division by 0")
+                        if 
