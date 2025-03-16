7eb3b54645066b4998d35fa121241be48161a54e
Clemuch009 <clementmuchaimoko@gmail.com>
2025-03-16 21:53:23 +0300

    more class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,15 +40,15 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("eight must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
         """ returns the are of the rectangle """
-        return self.__widht * self.__height
+        return self.__width * self.__height
 
     def perimeter(self):
         """ returns the perimeter of the rectangle """
-        if self.__width or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
