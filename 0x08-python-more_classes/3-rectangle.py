599002e61db78df2802ec45a5e264c62f414ca01
Clemuch009 <clementmuchaimoko@gmail.com>
2025-03-17 00:27:54 +0300

    more class

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
 
     def perimeter(self):
         """ returns the perimeter of the rectangle """
-        if self.__width or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__height + self.__width)
 
