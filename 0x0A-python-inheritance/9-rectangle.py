9f1c64c98badabcb9c572edd60acc5fc75b8cd78
Clemuch009 <clementmuchaimoko@gmail.com>
2025-03-21 22:24:59 +0300

    inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -12,17 +12,17 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """ validate value argument """
         if not isinstance(value, int):
-            raise TypeError(f"{value} must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value < 0:
-            raise ValueError(f"{value} must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
     """ class Rectangle that inherits from BaseGeometry """
     def __init__(self, width, height):
         """ initializes attributes """
-        integer_validator('width', width)
-        integer_validator('height', height)
+       self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__height = height
         self.__width = width
 
@@ -32,4 +32,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """ returns the description of the rectangle """
-        return (f" [Rectangle] {self.__height}/{self.__height}")
+        return (f" [Rectangle] {self.__width}/{self.__height}")
